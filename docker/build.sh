79ce89e29a753e843eb4877278512294eabb2f5e
Robert Vickerstaff <robert.vickerstaff@emr.ac.uk>
2016-02-29 15:19:46 +0000

    debugging docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -2,9 +2,29 @@
 
 #
 # build the docker image
-# run from with in the docker context directory
+#
 
-#get a fresh copy of crosslink files
+BUILD_DIR=/home/vicker/rjv_files/docker/docker_crosslink_build
 
+set -eu
 
-docker build -t rjvickerstaff/crosslink:0.1 .
+rm -rf ${BUILD_DIR}
+
+mkdir -p ${BUILD_DIR}
+
+cd ${BUILD_DIR} || exit
+
+#get up to date copies of crosslink and rjvbio
+git clone https://github.com/robertvi/rjvbio
+git clone https://github.com/eastmallingresearch/crosslink
+
+mkdir -p context
+
+mv ./crosslink ./context
+mv ./rjvbio ./context
+rm -rf ./context/crosslink/.git
+rm -rf ./context/rjvbio/.git
+
+cp ./context/crosslink/docker/Dockerfile context
+
+docker build -t rjvickerstaff/crosslink:0.1 context
