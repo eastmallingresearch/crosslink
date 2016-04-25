0c40949dcf139a0eea9556beba2adec4deb01368
Robert Vickerstaff <robert.vickerstaff@emr.ac.uk>
2016-04-25 17:09:12 +0100

    trying to fix the dbus error message which appears in docker container when running graphical programs

--- docker/build.sh
+++ docker/build.sh
@@ -8,16 +8,16 @@ BUILD_DIR=/home/vicker/rjv_files/docker/docker_crosslink_build
 
 set -eu
 
-#rm -rf ${BUILD_DIR}
-#mkdir -p ${BUILD_DIR}
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR} || exit
-#mkdir -p context
+mkdir -p context
 
 #get up to date copy of crosslink
-#git clone https://github.com/eastmallingresearch/crosslink
-#mv ./crosslink ./context
-#rm -rf ./context/crosslink/.git
-#cp ./context/crosslink/docker/Dockerfile context
+git clone https://github.com/eastmallingresearch/crosslink
+mv ./crosslink ./context
+rm -rf ./context/crosslink/.git
+cp ./context/crosslink/docker/Dockerfile context
 cp ~/rjv_mnt/cluster/git_repos/crosslink/docker/Dockerfile context
 
 docker build -t rjvickerstaff/crosslink:0.1 context
