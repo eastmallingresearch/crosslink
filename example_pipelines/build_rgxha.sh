97fb2bf3e8241d76940109fa6c6796cddc7502c7
Robert Vickerstaff <robert.vickerstaff@emr.ac.uk>
2016-06-08 11:43:41 +0100

    added more progress indication to simplified rgxha map script

--- example_pipelines/build_rgxha.sh
+++ example_pipelines/build_rgxha.sh
@@ -68,23 +68,37 @@ echo grouping...
 #initial exploratory grouping
 cl_group.sh   all.loc   initgrps   7.0
 
+echo fix typing errors...
+
 #fix maternal/paternal marker typing errors
 cl_fixtypes.sh   all.loc   all.loc   conf/fixtypes.000 
+
+echo grouping...
 
 #exploratory grouping after fixing type errors
 cl_group.sh   all.loc   fixgrps   7.0 
 
+echo find redundant markers...
+
 #generate list of non redundant markers
 cl_findredun.sh   all.loc   all.redun   conf/findredun.000 
+
+echo impute missing values...
 
 #impute missing values in all markers (including redundant ones)
 cl_knnimpute.sh   all.loc   all.loc   conf/knnimpute.000 
 
+echo extract non-redundant markers...
+
 #extract only the nonredundant imputed markers
 cl_extract.sh   all.loc   all.redun   all.uniq 
 
+echo grouping...
+
 #form linkage groups
 cl_group.sh   all.uniq   uniqgrps   7.0 
+
+echo phasing...
 
 #force phasing to complete down to a LOD of zero, even for falsely joined groups
 cl_phase.sh   uniqgrps   phasegrps 
@@ -97,8 +111,12 @@ cl_detect_crosslg.sh   phasegrps   crosslg_markers   conf/detectcrosslg.000
 #filter out cross linkage group markers
 cl_removemarkers.sh   all.uniq   filt.uniq   crosslg_markers
 
+echo grouping...
+
 #form linkage groups
 cl_group.sh   filt.uniq   filtgrps   7.0 
+
+echo phasing...
 
 #force phasing to complete
 cl_phase.sh   filtgrps   filtgrps 
@@ -106,13 +124,17 @@ cl_phase.sh   filtgrps   filtgrps
 #manually specify another cross linkage group marker
 echo 'PHR11-89834490' >> crosslg_markers
 
+echo remove cross linkage group markers...
+
 #filter out cross linkage group markers
 cl_removemarkers.sh   filt.uniq   filt.uniq   crosslg_markers
 
-echo regroup...
+echo group...
 
 #form linkage groups
 cl_group.sh   filt.uniq   filtgrps2   7.0
+
+echo phase...
 
 #force phasing to complete
 cl_phase.sh   filtgrps2   filtgrps2
@@ -122,16 +144,20 @@ echo finalise order...
 #produce final map order
 cl_order_hkimpute.sh   filtgrps2   finalgrps   conf/orderhkimpute.000
 
+echo finalise map positions...
+
 #calc map positions
 cl_mappos.sh   finalgrps   finalgrps 
 
-echo reinsert redundant loci
+echo reinsert redundant loci...
 
 #reinsert redundant markers into genotype files
 cl_reinsert_loc.sh   finalgrps   all.loc   all.redun   finalredun   conf/reinsert.000 
 
 #reinsert redundant markers into map files
 cl_reinsert_map.sh   finalgrps   all.redun   finalredun
+
+echo convert to joinmap...
 
 #convert to joinmap compatible files
 mkdir -p joinmap
